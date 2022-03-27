58a083ca313ee54ed2bcffaa46c64f7a4b2ffcec
wjervis7 <32655115+wjervis7@users.noreply.github.com>
2022-03-27 01:53:35 -0700

    Update 20220327083455_AnonymousSoftRemove.cs

--- 09.Mass.Extinction.Data/Migrations/20220327083455_AnonymousSoftRemove.cs
+++ 09.Mass.Extinction.Data/Migrations/20220327083455_AnonymousSoftRemove.cs
@@ -16,46 +16,6 @@ namespace _09.Mass.Extinction.Web.Data.Migrations
                 defaultValue: false,
                 oldClrType: typeof(bool),
                 oldType: "bit");
-
-            migrationBuilder.DropPrimaryKey("PK_AspNetUserTokens", "AspNetUserTokens");
-            migrationBuilder.AlterColumn<string>(
-                name: "Name",
-                table: "AspNetUserTokens",
-                type: "nvarchar(128)",
-                maxLength: 128,
-                nullable: false,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(450)");
-            migrationBuilder.AddPrimaryKey("PK_AspNetUserTokens", "AspNetUserTokens", "Name");
-
-            migrationBuilder.AlterColumn<string>(
-                name: "LoginProvider",
-                table: "AspNetUserTokens",
-                type: "nvarchar(128)",
-                maxLength: 128,
-                nullable: false,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(450)");
-            
-            migrationBuilder.DropPrimaryKey("PK_AspNetUserLogins", "AspNetUserLogins");
-            migrationBuilder.AlterColumn<string>(
-                name: "ProviderKey",
-                table: "AspNetUserLogins",
-                type: "nvarchar(128)",
-                maxLength: 128,
-                nullable: false,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(450)");
-            migrationBuilder.AddPrimaryKey("PK_AspNetUserLogins", "AspNetUserLogins", "ProviderKey");
-
-            migrationBuilder.AlterColumn<string>(
-                name: "LoginProvider",
-                table: "AspNetUserLogins",
-                type: "nvarchar(128)",
-                maxLength: 128,
-                nullable: false,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(450)");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
@@ -68,42 +28,6 @@ namespace _09.Mass.Extinction.Web.Data.Migrations
                 oldClrType: typeof(bool),
                 oldType: "bit",
                 oldDefaultValue: false);
-
-            migrationBuilder.AlterColumn<string>(
-                name: "Name",
-                table: "AspNetUserTokens",
-                type: "nvarchar(450)",
-                nullable: false,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(128)",
-                oldMaxLength: 128);
-
-            migrationBuilder.AlterColumn<string>(
-                name: "LoginProvider",
-                table: "AspNetUserTokens",
-                type: "nvarchar(450)",
-                nullable: false,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(128)",
-                oldMaxLength: 128);
-
-            migrationBuilder.AlterColumn<string>(
-                name: "ProviderKey",
-                table: "AspNetUserLogins",
-                type: "nvarchar(450)",
-                nullable: false,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(128)",
-                oldMaxLength: 128);
-
-            migrationBuilder.AlterColumn<string>(
-                name: "LoginProvider",
-                table: "AspNetUserLogins",
-                type: "nvarchar(450)",
-                nullable: false,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(128)",
-                oldMaxLength: 128);
         }
     }
 }
